5dd23035fad4f79fb278c919ffc69b52c84751a8
Elizabeth Borbon <elyborboon@gmail.com>
2024-11-10 16:22:15 -0600

    Add files via upload

---
+++ Ejercico 5 Def.py
@@ -0,0 +1,8 @@
+#Programa que convierta una unidad en dias, horas, minutos, segundos a segundos 
+def dias (dia):
+    tiempo_h = (dia * 24) 
+    tiempo_m = tiempo_h * 60 
+    tiempo_s = tiempo_m *60 
+    print (tiempo_s)
+dias (2)
+
